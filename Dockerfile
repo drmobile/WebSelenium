c054464553769a25eb7ebefaab890384247d6815
Jonas Cheng <jonas.cheng@gmail.com>
2018-09-17 22:19:18 +0800

    fix docker issue

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,14 @@
 FROM joyzoursky/python-chromedriver:2.7-selenium
 
-WORKDIR /var/WebSelenium
-
 RUN easy_install pip
 COPY ./requirements.txt /tmp/requirements.txt
 RUN pip install -r /tmp/requirements.txt
+
+# install Code2000 Font Set
+WORKDIR /root/.fonts
+RUN wget https://gschoppe.com/projects/fbformat/includes/unicodefont.zip; unzip unicodefont.zip; rm unicodefont.zip
+RUN fc-cache -fv
+
+WORKDIR /var/WebSelenium
 
 COPY . .
--- public/base.py
+++ public/base.py
@@ -1,9 +1,13 @@
 #coding=utf-8
 
-from selenium import webdriver
-import unittest
 import sys
 import logging
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 # logger
 logger = logging.getLogger()
@@ -14,23 +18,29 @@ ch.setFormatter(logFormatter)
 logger.addHandler(ch)
 logger.setLevel(logging.INFO)
 
+
 class BaseTests(unittest.TestCase):
     """
     The base class is for all testcase.
     """
     def setUp(self):
         options = webdriver.ChromeOptions()
-        # options.add_argument('--headless')
-        # options.add_argument('--no-sandbox')
-        # options.add_argument('--disable-gpu')
-
+        # default window size
+        options.add_argument('--window-size=1280x800')
+        # default browser lang
+        options.add_argument('--lang=zh-TW')
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--user-agent="Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Mobile Safari/537.36"')
         self.driver = webdriver.Chrome(options=options)
         self.driver.implicitly_wait(10)
+
         self.driver.get("https://www-staging.soocii.me")
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'menu')))
+        
         self.action = webdriver.ActionChains(self.driver)
         
     def tearDown(self):
-
+        self.driver.save_screenshot('{}.png'.format(__name__))
         self.driver.close()
-
         self.driver.quit()
