9d849a1b028f6385af630255284f736a59ecc478
Jonas Cheng <jonas.cheng@gmail.com>
2018-09-18 16:08:52 +0800

    disable gpu

--- public/base.py
+++ public/base.py
@@ -31,9 +31,10 @@ class BaseTests(unittest.TestCase):
         options.add_argument('--lang=zh-TW')
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
+        options.add_argument('--disable-gpu')
         options.add_argument('--user-agent="Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Mobile Safari/537.36"')
         self.driver = webdriver.Chrome(options=options)
-        self.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(30)
 
         self.driver.get("https://www-staging.soocii.me")
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'menu')))
